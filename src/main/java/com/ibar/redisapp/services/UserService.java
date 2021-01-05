6cb2bbde9e5a3ef886b5eef2f5679d13c6f360aa
004449 <sevinc.sarayeva@ibar.az>
2021-01-05 22:30:14 +0400

    minor changes

--- src/main/java/com/ibar/redisapp/services/UserService.java
+++ src/main/java/com/ibar/redisapp/services/UserService.java
@@ -6,7 +6,6 @@ import com.ibar.redisapp.entities.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface UserService {
